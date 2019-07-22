ca32333d637f8e1a510d9cbb217b0cde407c887c
SUNIL <SUNIL@192.168.1.3>
2019-07-22 19:54:34 +0530

    Testng groups,Suite files, Parameters

---
+++ Project_164/src/framework_testng/BeforeTest_and_AfterTest/Test_for_Chrome.java
@@ -0,0 +1,24 @@
+package framework_testng.BeforeTest_and_AfterTest;
+
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import org.testng.annotations.AfterTest;
+
+public class Test_for_Chrome
+{
+  
+  @BeforeTest
+  public void beforeTest() 
+  {
+	  System.out.println("Pretest before launch chrome");
+  }
+
+  @AfterTest
+  public void afterTest() 
+  {
+	  System.out.println("post after launch chrome");
+  }
+  
+
+
+}
